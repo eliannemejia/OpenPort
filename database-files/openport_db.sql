44be4a24a35cf281c9fffe7ffa06d2fd3334acc5
DroidBait66 <Alpbberrak@gmail.com>
2025-05-26 23:00:23 +0200

    Finished writing the tables with a few notes

--- database-files/openport_db.sql
+++ database-files/openport_db.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS ProBonoOpportunity (
 CREATE TABLE IF NOT EXISTS Spending (
   CountryID int PRIMARY KEY,
   PercentGDP DECIMAL(5,2),
-  Year int,
+  aYear YEAR,
   FOREIGN KEY (CountryID) REFERENCES Country (CountryID)
 );
 
@@ -68,5 +68,54 @@ CREATE TABLE IF NOT EXISTS AsylumSeeker (
   Religion int,
   FOREIGN KEY (CurrentLocation) REFERENCES Country (CountryID),
   FOREIGN KEY (UserID) REFERENCES User (userID),
-  FOREIGN KEY 
-)
+  FOREIGN KEY (AssignedLawyer) REFERENCES Lawyer (LawyerID),
+  FOREIGN KEY (Religion) REFERENCES Religion (ReligionID)
+);
+
+CREATE TABLE IF NOT EXISTS Lawyer (
+  LawyerID int PRIMARY KEY,
+  UserID int,
+  Nationality int,
+  PrefferedRegion int,
+  Specialization VarChar(100),
+  FOREIGN KEY (Nationality) REFERENCES Country (CountryID),
+  FOREIGN KEY (USerID) REFERENCES User (UserID)
+);
+
+CREATE TABLE IF NOT EXISTS Education (
+  LevelID int,
+  CountryID int,
+  Type VARCHAR (50),
+  AccessScore int,
+  Ranking int,
+  TotalStudents int,
+  FOREIGN KEY (CountryID) REFERENCES Country (CountryID),
+  FOREIGN KEY (LevelID) REFERENCES EducationLevel (LevelID),
+  PRIMARY KEY (CountryID, LevelID)
+);
+
+CREATE TABLE IF NOT EXISTS EducationLevel(
+  LevelID int PRIMARY KEY,
+  Name VarChar(100);
+);
+
+CREATE TABLE IF NOT EXISTS Decision (
+  DecisionID int,
+  DecidingCountry int,
+  ApplicantGroup int,
+  Count int, 
+  Type VARCHAR(1), /*maybe set a binary for this one?*/
+  aYear YEAR,
+  FOREIGN KEY (DecidingCountry) REFERENCES Country (CountryID),
+  FOREIGN KEY (ApplicantGroup) REFERENCES ApplicantGroup (GroupID),
+  PRIMARY KEY (DecisionID, CountryID, ApplicantGroup)
+);
+
+CREATE TABLE IF NOT EXISTS ApplicantGroup(
+  GroupID int PRIMARY KEY,
+  CountryID int,
+  AgeRange VARCHAR(10), /*has to be a better way to save this b/c it will be hard to query. 
+  maybe add a min and max age for age groups?*/
+  Sex VARCHAR(1),
+  FOREIGN KEY (CountryID) REFERENCES Country (CountryID) 
+);
