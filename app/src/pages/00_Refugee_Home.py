47ccc84edf08256df8799fa01e35261d39111ce8
DroidBait66 <Alpbberrak@gmail.com>
2025-06-03 23:23:35 +0200

    starting changes to pages

---
+++ app/src/pages/00_Refugee_Home.py
@@ -0,0 +1,35 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout = 'wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+st.title(f"Welcome, {st.session_state['first_name']}.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
+
+if st.button('Find Your Best Asylum Location', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/01_World_Bank_Viz.py')
+
+if st.button('See Education Data', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/02_Map_Demo.py')
+  
+if st.button('See Religious Demographics', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/02_Map_Demo.py')
+  
+if st.button('Seek Legal Aid', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/02_Map_Demo.py')
