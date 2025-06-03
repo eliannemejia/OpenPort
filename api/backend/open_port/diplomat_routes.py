7bf83eb46c1cbffe15d6bb907bcff7dd96baaabd
DroidBait66 <66795946+DroidBait66@users.noreply.github.com>
2025-06-03 22:52:57 +0200

    Revert "added route files"

--- api/backend/open_port/diplomat_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify, request
-from backend.db_connection import db
-from mysql.connector import Error
-from flask import current_app
-
-# Create a Blueprint for NGO routes
-diplomats = Blueprint("diplomats", __name__)
-
