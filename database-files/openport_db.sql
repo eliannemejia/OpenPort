70ebae58729fc924d539e5b1f3300e4e86ad8fc2
DroidBait66 <Alpbberrak@gmail.com>
2025-06-04 21:55:07 +0200

    added aid project table

--- database-files/openport_db.sql
+++ database-files/openport_db.sql
@@ -131,6 +131,15 @@ CREATE TABLE IF NOT EXISTS LegalAidApplication (
   FOREIGN KEY (UserID) REFERENCES AsylumSeeker (ApplicantID)
 );
 
+CREATE TABLE IF NOT EXISTS AidProject (
+  ProjectID INT AUTO_INCREMENT PRIMARY KEY,
+  CountryID INT,
+  StartDate DATE,
+  Title VARCHAR(100),
+  Proj_Description VARCHAR(300),
+  FOREIGN KEY (CountryID) REFERENCES Country(CountryID)
+);
+
 INSERT INTO Country (CountryName) VALUES
 ('Belgium'),
 ('Bulgaria'),
