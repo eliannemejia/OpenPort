508dc8e69e3945feb45d657215d7fbb6ef9f12ad
eliannemejia <eliannemejia04@gmail.com>
2025-06-05 16:58:15 +0200

    connected log regression model to route

--- api/backend/ml_models/log_reg.py
+++ api/backend/ml_models/log_reg.py
@@ -1,6 +1,7 @@
 import json
 import pandas as pd
 import numpy as np
+from flask import current_app
 
 def logistic_regression(X, y, w, alpha = 1, max_iter = None):
     """
@@ -46,11 +47,13 @@ def predict_logistic(x, w, pred0 = True):
     if pred0 == True:
         yhat = 1/(1 + np.exp(np.dot(x, w)))
     else:
+        current_app.logger.info(f'x = {x}, w = {w}')
         yhat = np.exp(np.dot(x, w))/(1 + np.exp(np.dot(x, w)))
     return yhat
 
 
-def predict_acceptance(age, sex, citizen, one_hot_template, w):
+
+def predict_acceptance(age, sex, citizen, geo, one_hot_template, w):
     input_vector = np.zeros(one_hot_template.shape[1])
     col_names = one_hot_template.columns
 
@@ -60,8 +63,11 @@ def predict_acceptance(age, sex, citizen, one_hot_template, w):
         input_vector[col_names.get_loc(f"sex_{sex}")] = 1
     if f"citizen_{citizen}" in col_names:
         input_vector[col_names.get_loc(f"citizen_{citizen}")] = 1
-
+    if f"geo_{geo}" in col_names:
+        input_vector[col_names.get_loc(f"geo_{geo}")] = 1
+    
     input_vector = np.insert(input_vector, 0, 1)
-
+    
+    current_app.logger.info(f'input_vector = {input_vector}')
     yhat = predict_logistic(input_vector, w, pred0=False)
-    return yhat
+    return yhat.tolist()
