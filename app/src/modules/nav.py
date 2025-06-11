84248daf28a98ec535dd44b0a8d49952d8c4357e
eliannemejia <eliannemejia04@gmail.com>
2025-06-11 22:37:06 +0200

    combined religioius and education stats pages

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -31,65 +31,16 @@ def LegalAidHome():
 def AcceptanceProbStats():
     st.sidebar.page_link("pages/01_App_Prob_Stats.py", label="Find Best Asylum Option", icon="✅")
 
-def EducationSysData():
-    st.sidebar.page_link("pages/03_Education_Data.py", label="See Education Stats", icon="📚")
-    
-def ReligDemographics():
-    st.sidebar.page_link("pages/04_Religious_Demo.py", label="See Religious Demographics", icon="📊")
+def CountryDemographics():
+    st.sidebar.page_link("pages/03_Country_Demographics.py", label="See Country Demographics", icon="📚")
     
 def LegalAidHome():
     st.sidebar.page_link("pages/02_Legal_Aid_App.py", label="Seek Legal Aid", icon="👨🏽‍⚖️")
     
 def RefugeeProfile():
     st.sidebar.page_link("pages/06_Refugee_Profile.py", label="User Profile", icon="👤")
-#### ------------------------ Examples for Role of pol_strat_advisor ------------------------
-def PolStratAdvHomeNav():
-    st.sidebar.page_link(
-        "pages/00_Pol_Strat_Home.py", label="Political Strategist Home", icon="👤"
-    )
+    
 
-
-def WorldBankVizNav():
-    st.sidebar.page_link(
-        "pages/01_World_Bank_Viz.py", label="World Bank Visualization", icon="🏦"
-    )
-
-
-def MapDemoNav():
-    st.sidebar.page_link("pages/02_Map_Demo.py", label="Map Demonstration", icon="🗺️")
-
-
-## ------------------------ Examples for Role of usaid_worker ------------------------
-def ApiTestNav():
-    st.sidebar.page_link("pages/12_API_Test.py", label="Test the API", icon="🛜")
-
-
-def PredictionNav():
-    st.sidebar.page_link(
-        "pages/11_Prediction.py", label="Regression Prediction", icon="📈"
-    )
-
-
-def ClassificationNav():
-    st.sidebar.page_link(
-        "pages/13_Classification.py", label="Classification Demo", icon="🌺"
-    )
-
-
-def NgoDirectoryNav():
-    st.sidebar.page_link("pages/14_NGO_Directory.py", label="NGO Directory", icon="📁")
-
-
-def AddNgoNav():
-    st.sidebar.page_link("pages/15_Add_NGO.py", label="Add New NGO", icon="➕")
-
-
-#### ------------------------ System Admin Role ------------------------
-def AdminPageNav():
-    st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon="🖥️")
-    st.sidebar.page_link(
-        "pages/21_ML_Model_Mgmt.py", label="ML Model Management", icon="🏢"
-    )
 def DiplomatNav():
     st.sidebar.page_link("pages/20_Commissioner_Home.py", label="Diplomat Home", icon="👤")
     st.sidebar.page_link("pages/21_pending_to_aid.py", label="Pending/Aid", icon="⛑️")
@@ -123,28 +74,9 @@ def SideBarLinks(show_home=False):
         # Show Asylum Acceptance Probability, Education Data, Religious Demographics and Legal Application Links
         if st.session_state["role"] == "asylum_seeker":
             AcceptanceProbStats()
-            EducationSysData()
-            ReligDemographics()
+            CountryDemographics()
             LegalAidHome()
             RefugeeProfile()
-            
-        # # Show World Bank Link and Map Demo Link if the user is a political strategy advisor role.
-        # if st.session_state["role"] == "pol_strat_advisor":
-        #     PolStratAdvHomeNav()
-        #     WorldBankVizNav()
-        #     MapDemoNav()
-
-        # # If the user role is usaid worker, show the Api Testing page
-        # if st.session_state["role"] == "usaid_worker":
-        #     PredictionNav()
-        #     ApiTestNav()
-        #     ClassificationNav()
-        #     NgoDirectoryNav()
-        #     AddNgoNav()
-
-        # If the user is an administrator, give them access to the administrator pages
-        if st.session_state["role"] == "administrator":
-            AdminPageNav()
         
         if st.session_state["role"] == "diplomat":
             DiplomatNav()
