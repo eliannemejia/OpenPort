b143a5bd19a0b461e4856c001437f669d074832b
DroidBait66 <Alpbberrak@gmail.com>
2025-06-12 11:54:41 +0200

    shorten button name

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -44,8 +44,8 @@ def RefugeeProfile():
 def DiplomatNav():
     st.sidebar.page_link("pages/20_Commissioner_Home.py", label="Commissioner Home", icon="👤")
     st.sidebar.page_link("pages/21_pending_to_aid.py", label="Pending Funding", icon="💰")
-    st.sidebar.page_link("pages/22_Low_Acpt.py", label="Decision Stats Per Capita", icon="📈")
-    st.sidebar.page_link("pages/23_Group_Acpt.py", label="Expenditure Projections", icon="🏛️")
+    st.sidebar.page_link("pages/22_Low_Acpt.py", label="Decision Stats", icon="📈")
+    st.sidebar.page_link("pages/23_Group_Acpt.py", label="Future Expenditure", icon="🏛️")
     st.sidebar.page_link("pages/24_Find_Aid_Projects.py", label="Aid Dashboard", icon="🌍")
 
 
